fd23e66f181fb659c6190860bc498358606d323b
jordanmmartin <jdordan23@gmail.com>
2019-05-12 15:56:33 -0400

    converted todo from a functional to class component

--- src/client/components/todo.js
+++ src/client/components/todo.js
@@ -9,62 +9,77 @@ import { mdiCheck } from '@mdi/js'
 
 const noop = () => {};
 
-
+const baseCls = 'todo';
 /**
  * Prop Types
  * @private
  */
-const propTypes = {
-  filtered: PropTypes.bool,
-  onClickDelete: PropTypes.func,
-  onClickTodo: PropTypes.func,
-  status: PropTypes.string,
-  text: PropTypes.string,
-};
+// const propTypes = {
+//   filtered: PropTypes.bool,
+//   onClickDelete: PropTypes.func,
+//   onClickTodo: PropTypes.func,
+//   status: PropTypes.string,
+//   text: PropTypes.string,
+// };
 
 /**
  * Default Props
  * @private
  */
-const defaultProps = {
-  filtered: false,
-  onClickDelete: noop,
-  onClickTodo: noop,
-  status: '',
-  text: '',
-};
+// const defaultProps = {
+//   filtered: false,
+//   onClickDelete: noop,
+//   onClickTodo: noop,
+//   status: '',
+//   text: '',
+// };
 
 /**
  * Todo component
  * @returns {ReactElement}
  */
-const Todo = ({ filtered, onClickDelete, onClickTodo, status, text }) => {
+// const Todo = ({ filtered, onClickDelete, onClickTodo, status, text }) => {
+class Todo extends React.Component {
   /**
    * Base CSS class
    */
-  const baseCls = 'todo';
+  // static baseCls = 'todo';
 
-  const todoCls = baseCls
-    + (status === 'complete' ? ' todo--status-complete' : '')
-    + (filtered ? ' todo--filtered' : '');
+  static propTypes = {
+    filtered: PropTypes.bool,
+    onClickDelete: PropTypes.func,
+    onClickTodo: PropTypes.func,
+    status: PropTypes.string,
+    text: PropTypes.string,
+  };
 
-  return (
-    <li className={todoCls}>
-    <div className="pretty p-icon p-rotate">
-        <input type="checkbox" />
-        <div className="state p-success">
-            <i className="icon mdi mdi-check"></i>
-            <label></label>
+  // static todoCls = baseCls
+  //   + (this.props.status === 'complete' ? ' todo--status-complete' : '')
+  //   + (this.props.filtered ? ' todo--filtered' : '');
+
+  render(){
+    let baseCls = 'todo';
+    let todoCls = baseCls
+      + (this.props.status === 'complete' ? ' todo--status-complete' : '')
+      + (this.props.filtered ? ' todo--filtered' : '');
+    return (
+      <li className={todoCls}>
+        <div className="pretty p-icon p-rotate">
+            <input type="checkbox" />
+            <div className="state p-success">
+                <i className="icon mdi mdi-check"></i>
+                <label></label>
+            </div>
         </div>
-    </div>
-      <TodoLink text={text} onClick={onClickTodo} />
+        <TodoLink text={this.props.text} onClick={this.props.onClickTodo} />
 
-      <Button text="Delete" onClick={onClickDelete} />
-    </li>
-  );
+        <Button text="Delete" onClick={this.props.onClickDelete} />
+      </li>
+    );
+  }
 }
 
-Todo.propTypes = propTypes;
-Todo.defaultProps = defaultProps;
+// Todo.propTypes = propTypes;
+// Todo.defaultProps = defaultProps;
 
 export default Todo;
